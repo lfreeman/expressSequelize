9de7d1c3d83a742958891ed19f4ee5220c9ac261
Leonid Freeman <lfreeman@realnetworks.com>
2016-12-01 11:32:27 -0500

    basic version

---
+++ setuppassport.js
@@ -0,0 +1,58 @@
+var passport = require('passport');
+var models = require("./models");
+var Strategy = require('passport-local').Strategy;
+
+passport.use("login", new Strategy(
+    function (username, password, done) {
+        models.User.findOne({
+            where: {
+                username: username
+            }
+        }).then(function (user) {
+                return done(null, user);
+            }
+        )
+    }));
+
+
+//         , function (err, user) {
+//         if (err) {
+//             return done(err);
+//         }
+//         if (!user) {
+//             return done(null, false, {message: "No user has that username!"});
+//
+//         }
+//         user.checkPassword(password, function (err, isMatch) {
+//             if (err) {
+//                 return done(err);
+//             }
+//             if (isMatch) {
+//                 return done(null, user);
+//             } else {
+//                 return done(null, false,
+//                     {message: "Invalid password."});
+//             }
+//         });
+//     });
+// }));
+
+// models.User.findOne({
+//     where: {
+//         username: req.params.username
+//     }
+// }).then(function (user) {
+//     res.render("profile", {user: user})
+// })
+
+
+module.exports = function () {
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+    passport.deserializeUser(function (id, done) {
+        models.User.findById(id).then(function (user) {
+            done(null, user);
+        });
+    });
+};
